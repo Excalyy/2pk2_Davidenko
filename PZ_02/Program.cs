10a219cbe127290e7c9a1a8eb3070c442e784189
f7u46dv34590fdks@hotmail.com <f7u46dv34590fdks@hotmail.com>
2023-09-17 14:02:43 +0500

    добавил 3 практику

--- PZ_02/Program.cs
+++ PZ_02/Program.cs
@@ -1,10 +1,36 @@
-﻿namespace PZ_02
+﻿namespace PZ_02;
+class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        int c;
+        double d;
+        Console.Write("Введите значение c: ");
+        c = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Введите значение d: ");
+        d = Convert.ToDouble(Console.ReadLine());
+        double n, m, k;
+        if (c < 5)
         {
-            Console.WriteLine("Hello, World!");
+            n = Math.Pow(c, 2) * Math.Sin(c * d) + Math.Sqrt(Math.Abs(c));
         }
+        else
+        {
+            n = d * Math.Cos(c * d) + 1 / Math.Sqrt(Math.Abs(c));
+        }
+        if (n <= 2)
+        {
+            m = Math.Exp(c) + Math.Pow(Math.Sin(n), 2) + d;
+        }
+        else
+        {
+            m = c * Math.Cos(5 / n - 3);
+        }
+        k = 20 * d + 10 * n - m + n / 2;
+        Console.WriteLine($"Значение n: {n}");
+        Console.WriteLine($"Значение m: {m}");
+        Console.WriteLine($"Значение k: {k}");
+
+        Console.ReadLine();
     }
 }
---
+++ PZ_03/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace PZ_03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int day;
+            Console.Write(" Введите текущий день:  ");
+            if (!int.TryParse(Console.ReadLine(), out day) || day < 1 || day > 31)
+            { Console.WriteLine(" Некорректный ввод дня месяца"); return; }
+            int decade;
+            switch (day)
+            {
+                case int n when (n >= 1 && n <= 10):
+                    decade = 1; // первая декада
+                    break;
+                case int n when (n >= 11 && n <= 20):
+                    decade = 2; // вторая декада
+                    break;
+                case int n when (n >= 21 && n <= 31):
+                    decade = 3; // третья декада
+                    break;
+                default:
+                    decade = 0;
+                    break;
+            }
+            Console.WriteLine($"Число попадает в {decade} декаду месяца.");
+        }
+    }
+}
