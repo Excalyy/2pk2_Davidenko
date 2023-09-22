a9f719b0a395a083eff68b9b8ea8896bd7ee970d
f7u46dv34590fdks@hotmail.com <f7u46dv34590fdks@hotmail.com>
2023-09-22 18:42:29 +0500

    добавил 5 практику

---
+++ PZ_05/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace PZ_05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите целое число N:");
+            int N = int.Parse(Console.ReadLine());
+
+            bool isPowerOfThree = IsPowerOfThree(N);
+
+            Console.WriteLine(isPowerOfThree);
+        }
+
+        static bool IsPowerOfThree(int N)
+        {
+            while (N % 3 == 0)
+            {
+                N /= 3;
+            }
+
+            return N == 1;
+        }
+     }   
+}
