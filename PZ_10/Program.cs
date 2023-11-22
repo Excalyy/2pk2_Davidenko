ca5ea5960863c28f87b7da9246de7f3bb1de466e
f7u46dv34590fdks@hotmail.com <f7u46dv34590fdks@hotmail.com>
2023-11-22 18:03:34 +0500

    Добавил 10 практику

---
+++ PZ_10/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите текст:");
+        string inputText = Console.ReadLine();
+
+        string[] words = inputText.Split(' ');
+
+        string invertedText = "";
+
+        foreach (string word in words)
+        {
+            int wordLength = word.Length;
+            string invertedWord = "";
+
+            for (int i = wordLength - 1; i >= 0; i--)
+            {
+                invertedWord += word[i];
+            }
+
+            invertedText += invertedWord;
+
+            if (word != words[words.Length - 1])
+            {
+                invertedText += " ";
+            }
+        }
+
+        Console.WriteLine("Инвертированный текст:");
+        Console.WriteLine(invertedText);
+        Console.ReadLine();
+    }
+}
