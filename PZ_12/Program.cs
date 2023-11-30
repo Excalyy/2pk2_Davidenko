a3cbd9836f66beecfdd7f11c9a7f639a1b73fe7e
f7u46dv34590fdks@hotmail.com <f7u46dv34590fdks@hotmail.com>
2023-11-30 21:43:13 +0500

    Добавил 12 практику

---
+++ PZ_12/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+public class Program
+{
+    public static double[,] GenerateRandomArray(int a, int b)
+    {
+        double[,] result = new double[a, b];
+        Random random = new Random();
+
+        for (int i = 0; i < a; i++)
+        {
+            for (int j = 0; j < b; j++)
+            {
+                double randomValue = Math.Round(random.NextDouble() * 20 - 10, 2);
+                result[i, j] = randomValue;
+            }
+        }
+
+        return result;
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine("Введите количество рядов (a): ");
+        int a = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите количество столбцов (b): ");
+        int b = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Итоговая таблица: ");
+        double[,] array = GenerateRandomArray(a, b);
+
+        for (int i = 0; i < a; i++)
+        {
+            for (int j = 0; j < b; j++)
+            {
+                Console.Write(array[i, j] + " ");
+            }
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
