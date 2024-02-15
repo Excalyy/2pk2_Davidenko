7b2815e8f6887c1bde7568e03d5759064722f727
f7u46dv34590fdks@hotmail.com <f7u46dv34590fdks@hotmail.com>
2024-02-15 19:38:13 +0500

    добавил 18 практику

---
+++ PZ_18/Patient.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+
+    public class Patient
+    {
+        private static int count = 0;
+
+        public string FullName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public DateTime AdmissionDate { get; set; }
+        public DateTime DischargeDate { get; private set; }
+
+        public Patient(string fullName, DateTime birthDate, DateTime admissionDate)
+        {
+            if (string.IsNullOrWhiteSpace(fullName))
+            {
+                throw new ArgumentException("ФИО пациента не может быть пустым");
+            }
+
+            if (birthDate > DateTime.Today.AddYears(-18))
+            {
+                throw new ArgumentException("Пациенту должно быть 18 лет или больше");
+            }
+
+            if (admissionDate < DateTime.Today.AddDays(-7))
+            {
+                throw new ArgumentException("Дата поступления не может быть ранее, чем неделю назад");
+            }
+
+            if (count >= 10)
+            {
+                throw new InvalidOperationException("Отделение заполнено, невозможно принять нового пациента");
+            }
+
+            FullName = fullName;
+            BirthDate = birthDate.Date;
+            AdmissionDate = admissionDate.Date;
+            DischargeDate = default(DateTime);
+
+            count++;
+        }
+
+        public bool IsAdult()
+        {
+            return BirthDate <= DateTime.Today.AddYears(-18);
+        }
+
+        public void Discharge()
+        {
+            if (DischargeDate == default(DateTime))
+            {
+                DischargeDate = DateTime.Today;
+                count--;
+                Console.WriteLine($"Пациент {FullName} поступил в отделение {AdmissionDate.ToString("dd.MM.yy")}, выписан {DischargeDate.ToString("dd.MM.yy")}");
+            }
+        }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine($"ФИО: {FullName}");
+            Console.WriteLine($"Дата рождения: {BirthDate.ToString("dd.MM.yy")}");
+            Console.WriteLine($"Дата поступления: {AdmissionDate.ToString("dd.MM.yy")}");
+        }
+
+        public static void PrintPatientCount()
+        {
+            Console.WriteLine($"Количество пациентов в отделении: {count}");
+        }
+    }
+}
---
+++ PZ_18/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var patient1 = new Patient("Иванов Иван Иванович", new DateTime(1990, 1, 1), DateTime.Today.AddDays(-5));
+                var patient2 = new Patient("Петров Петр Петрович", new DateTime(2005, 5, 5), DateTime.Today.AddDays(-3));
+                var patient3 = new Patient("Сидоров Сидор Сидорович", new DateTime(1985, 10, 10), DateTime.Today.AddDays(-2));
+
+                patient1.PrintInfo();
+                patient2.PrintInfo();
+                patient3.PrintInfo();
+
+                if (patient1.IsAdult())
+                {
+                    Console.WriteLine($"{patient1.FullName} является совершеннолетним");
+                }
+                else
+                {
+                    Console.WriteLine($"{patient1.FullName} не является совершеннолетним");
+                }
+
+                patient1.Discharge();
+                patient2.Discharge();
+                patient3.Discharge();
+
+                Patient.PrintPatientCount();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
